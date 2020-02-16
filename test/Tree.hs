15acfcf9c68d75f89d16cf7aa0d829bde8d54b85
Kirill Elagin <kirelagin@gmail.com>
2020-02-15 21:12:08 -0500

    Add missing licensing information

--- test/Tree.hs
+++ test/Tree.hs
@@ -1,1 +1,6 @@
+{- SPDX-FileCopyrightText: 2020 Serokell <https://serokell.io/>
+ -
+ - SPDX-License-Identifier: MPL-2.0
+ -}
+
 {-# OPTIONS_GHC -F -pgmF tasty-discover -optF --tree-display -optF --generated-module -optF Tree #-}
