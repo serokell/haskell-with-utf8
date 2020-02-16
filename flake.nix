cd67a3183e96d1705bbc528cad40b894df7200e8
Kirill Elagin <kirelagin@gmail.com>
2020-02-15 21:23:42 -0500

    Build with haskell.nix

--- flake.nix
+++ flake.nix
@@ -7,14 +7,27 @@
 
   description = "Get your UTF-8 IO right on the first try";
 
-  outputs = { self, nixpkgs }:
-    {
+  outputs = { self, nixpkgs, haskell-nix }:
+    let
+      pkgs = nixpkgs {
+        overlays = haskell-nix.overlays;
+      };
+
+      project = pkgs.haskell-nix.stackProject {
+        src = pkgs.haskell-nix.haskellLib.cleanGit {
+          name = "haskell-utf8";
+          src = ./.;
+        };
+      };
+      utf8 = project.utf8;
+    in {
       packages = {
-        haskell-utf8 = null;
+        haskell-utf8 = utf8.components.library;
       };
 
       checks = {
         build = self.packages.haskell-utf8;
+        test = utf8.checks.utf8-test;
       };
     };
 }
