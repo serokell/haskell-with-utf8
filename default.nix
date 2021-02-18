c916ad47c39be3b46f8bfa23a5faa7673e2e8c35
Kirill Elagin <kirelagin@gmail.com>
2021-02-17 22:10:45 -0500

    Convert to a proper flake
    * Get rid of Niv
    * Repin haskell.nix
    * Repin nixpkgs

--- default.nix
+++ default.nix
@@ -1,27 +1,7 @@
-# SPDX-FileCopyrightText: 2020 Serokell <https://serokell.io/>
-#
-# SPDX-License-Identifier: MPL-2.0
-
-#
-# nix-flakes shim
-#
-
-let
-  sources = builtins.removeAttrs (import ./nix/sources.nix) ["__functor"];
-  # https://github.com/input-output-hk/haskell.nix/blob/master/lib/override-with.nix
-  tryOverride = override: default:
-    let
-      try = builtins.tryEval (builtins.findFile builtins.nixPath override);
-    in if try.success then
-      builtins.trace "using search host <${override}>" try.value
-       else
-         default;
-  inputs = builtins.mapAttrs (name: s: import (tryOverride "flake-${name}" s)) sources;
-  flake = (import ./flake.nix).outputs (inputs // { self = flake; });
-in
-{ exposeFlake ? false }:
-if exposeFlake then
-  flake
-else {
-  inherit (flake.packages) with-utf8;
-}
+(import (
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
+    sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2"; }
+) {
+  src =  ./.;
+}).defaultNix
--- flake.nix
+++ flake.nix
@@ -3,31 +3,42 @@
 # SPDX-License-Identifier: MPL-2.0
 
 {
-  edition = 201911;
-
   description = "Get your UTF-8 IO right on the first try";
 
-  outputs = { self, nixpkgs, haskell-nix }:
-    let
-      pkgs = nixpkgs {
-        overlays = haskell-nix.overlays;
-      };
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    haskell-nix.url = "github:input-output-hk/haskell.nix/ba87ffb84bc4158ada14908bddee9df2c81ac31b";
+    nixpkgs.url = "github:nixos/nixpkgs/f02bf8ffb9a5ec5e8f6f66f1e5544fd2aa1a0693";
+  };
 
-      project = pkgs.haskell-nix.stackProject {
-        src = pkgs.haskell-nix.haskellLib.cleanGit {
-          name = "with-utf8";
-          src = ./.;
+  outputs = { self, nixpkgs, flake-utils, haskell-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system}.extend haskell-nix.overlay;
+
+        project = pkgs.haskell-nix.stackProject {
+          src = pkgs.haskell-nix.haskellLib.cleanGit {
+            name = "with-utf8";
+            src = ./.;
+          };
         };
-      };
-      with-utf8 = project.with-utf8;
-    in {
-      packages = {
-        with-utf8 = with-utf8.components.library;
-      };
+        with-utf8 = project.with-utf8;
+      in rec {
+        packages = {
+          with-utf8 = with-utf8.components.library;
+        };
+        defaultPackage = packages.with-utf8;
 
-      checks = {
-        build = self.packages.with-utf8;
-        test = with-utf8.checks.with-utf8-test;
-      };
-    };
+        checks = {
+          build = packages.with-utf8;
+          test = with-utf8.checks.with-utf8-test;
+        };
+
+        apps.utf8-troubleshoot = {
+          type = "app";
+          program = "${with-utf8.components.exes.utf8-troubleshoot}/bin/utf8-troubleshoot";
+        };
+        defaultApp = apps.utf8-troubleshoot;
+      }
+    );
 }
