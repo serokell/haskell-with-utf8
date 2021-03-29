addd9ce5030988a01d8d409d9643a36925efc773
Kirill Elagin <kirelagin@gmail.com>
2021-03-29 08:36:54 -0400

    ci: Add checks for different GHC versions
    Repin haskell.nix and generate checks for multiple chosen versions of GHC.

--- flake.nix
+++ flake.nix
@@ -7,32 +7,35 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    haskell-nix.url = "github:input-output-hk/haskell.nix/ba87ffb84bc4158ada14908bddee9df2c81ac31b";
-    nixpkgs.url = "github:nixos/nixpkgs/f02bf8ffb9a5ec5e8f6f66f1e5544fd2aa1a0693";
+    haskell-nix.url = "github:input-output-hk/haskell.nix/43cb0fc8957be7ab027f8bd5d48bc22479032c1f";
+    nixpkgs.url = "github:serokell/nixpkgs/25cb6c920e31f80cc4c4559c840c5753d4a9012f";
   };
 
   outputs = { self, nixpkgs, flake-utils, haskell-nix }:
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = nixpkgs.legacyPackages.${system}.extend haskell-nix.overlay;
+        inherit (pkgs) lib;
 
-        project = pkgs.haskell-nix.stackProject {
-          src = pkgs.haskell-nix.haskellLib.cleanGit {
-            name = "with-utf8";
-            src = ./.;
-          };
-        };
-        with-utf8 = project.with-utf8;
+        src = ./.;
+        project = args: pkgs.haskell-nix.stackProject ({ inherit src; } // args);
+        with-utf8 = (project {}).with-utf8;
+        check = args: (project args).with-utf8.checks.with-utf8-test;
       in rec {
         packages = {
           with-utf8 = with-utf8.components.library;
         };
         defaultPackage = packages.with-utf8;
 
-        checks = {
-          build = packages.with-utf8;
-          test = with-utf8.checks.with-utf8-test;
-        };
+        checks =
+          let
+            mkGhcCheck = ghcName: {
+              name = "test-${ghcName}";
+              value = check { ghc = pkgs.haskell-nix.compiler.${ghcName}; };
+            };
+          in {
+            test = with-utf8.checks.with-utf8-test;
+          } // lib.listToAttrs (map mkGhcCheck [ "ghc884" "ghc8104" ]);
 
         apps.utf8-troubleshoot = {
           type = "app";
