1b6156f28620638bd2529b3ee57af32ffe2d3881
Kirill Elagin <kirelagin@gmail.com>
2021-11-03 17:23:52 -0400

    meta: Use haskell.nix flake lib

--- flake.nix
+++ flake.nix
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2020 Serokell <https://serokell.io/>
+# SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io/>
 #
 # SPDX-License-Identifier: MPL-2.0
 
@@ -10,6 +10,8 @@
   };
 
   inputs = {
+    serokell-nix.url = "github:serokell/serokell.nix/kirelagin/lib.haskell.makeFlake";
+    nixpkgs.url = "github:serokell/nixpkgs";
     haskell-nix = {
       inputs.hackage.follows = "hackage";
       inputs.stackage.follows = "stackage";
@@ -18,37 +20,19 @@
     stackage.flake = false;
   };
 
-  outputs = { self, nixpkgs, flake-utils, haskell-nix, hackage, stackage }:
+  outputs = { self, nixpkgs, flake-utils, serokell-nix, haskell-nix, hackage, stackage }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = nixpkgs.legacyPackages.${system}.extend haskell-nix.overlay;
-        inherit (pkgs) lib;
 
-        src = ./.;
-        project = args: pkgs.haskell-nix.stackProject ({ inherit src; } // args);
-        with-utf8 = (project {}).with-utf8;
-        check = args: (project args).with-utf8.checks.with-utf8-test;
-      in rec {
-        packages = {
-          with-utf8 = with-utf8.components.library;
-        };
-        defaultPackage = packages.with-utf8;
-
-        checks =
-          let
-            mkGhcCheck = ghcVersion: {
-              name = "test-ghc${ghcVersion}";
-              value = check { ghc = pkgs.haskell-nix.compiler."ghc${ghcVersion}"; };
-            };
-          in {
-            test = with-utf8.checks.with-utf8-test;
-          } // lib.listToAttrs (map mkGhcCheck [ "884" "8107" "901" ]);
-
-        apps.utf8-troubleshoot = {
-          type = "app";
-          program = "${with-utf8.components.exes.utf8-troubleshoot}/bin/utf8-troubleshoot";
-        };
-        defaultApp = apps.utf8-troubleshoot;
-      }
+        flake =
+          serokell-nix.lib.haskell.makeFlake pkgs.haskell-nix pkgs.haskell-nix.stackProject {
+            src = ./.;
+            ghcVersions = [ "884" "8107" "901" ];
+          };
+      in flake // {
+          defaultPackage = flake.packages."with-utf8:lib:with-utf8";
+          defaultApp = flake.apps."with-utf8:exe:utf8-troubleshoot";
+        }
     );
 }
